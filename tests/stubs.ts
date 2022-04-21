fd310616f78dab303b656e329197484459ee3179
Mati <matibat@gmail.com>
2022-04-21 18:06:51 -0300

    test(setup): copies the mocha setup from CORE PE-1364

---
+++ tests/stubs.ts
@@ -0,0 +1,20 @@
+import Arweave from 'arweave';
+import { ADDR, ArweaveAddress, TxID } from '../src/types';
+
+export const fakeArweave = Arweave.init({
+	host: 'localhost',
+	port: 443,
+	protocol: 'https',
+	timeout: 600000
+});
+
+export const stubArweaveAddress = (address = 'abcdefghijklmnopqrxtuvwxyz123456789ABCDEFGH'): ArweaveAddress => {
+	return ADDR(address);
+};
+
+export const stubTxID = TxID('0000000000000000000000000000000000000000001');
+export const stubCommunityContract = {
+	settings: [['fee', 50]],
+	vault: { [`${stubArweaveAddress()}`]: [{ balance: 500, start: 1, end: 2 }] },
+	balances: { [`${stubArweaveAddress()}`]: 200 }
+};
