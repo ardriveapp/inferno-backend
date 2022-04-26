b9060670015c95f9cfa04797e06bd4bde718d443
Mati <matibat@gmail.com>
2022-04-26 15:05:17 -0300

    feat(yargs): setup a command for data aggregation PE-1340

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,45 @@
+import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
+
 import { DailyOutput } from './daily_output';
 import { getAllTransactionsWithin, getWalletsEligibleForStreak } from './queries';
 
-const minBlock = 916705;
-const maxBlock = 916756;
-
 if (require.main === module) {
-	console.log('TESTING');
+	run();
+}
 
+function run(): void {
+	yargs(hideBin(process.argv)).command(
+		'aggregate [minBlock] [maxBlock]',
+		'Aggregates data into the output file',
+		(yargs) => {
+			return yargs
+				.positional('minBlock', {
+					describe: 'The block from where to start the query',
+					type: 'number'
+				})
+				.positional('maxBlock', {
+					describe: 'The last block to query',
+					type: 'number'
+				});
+		},
+		(argv) => {
+			const { minBlock, maxBlock } = argv;
+			if (!(minBlock && maxBlock)) {
+				throw new Error('You must specify the min and max block height to query');
+			}
+			aggregateOutputData(minBlock, maxBlock);
+		}
+	);
+
+	yargs.parse();
+}
+
+async function aggregateOutputData(minBlock: number, maxBlock: number): Promise<void> {
 	const output = new DailyOutput();
-	getWalletsEligibleForStreak().then((PSTHolders) => {
-		output.feedPSTHolders(PSTHolders);
-		getAllTransactionsWithin(minBlock, maxBlock).then((edges) => {
-			output.feedGQLData(edges);
-			output.write();
-		});
-	});
+	const PSTHolders = await getWalletsEligibleForStreak();
+	output.feedPSTHolders(PSTHolders);
+	const edges = await getAllTransactionsWithin(minBlock, maxBlock);
+	output.feedGQLData(edges);
+	output.write();
 }
