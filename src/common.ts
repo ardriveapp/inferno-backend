e3cdb0684ad89f0666d28b420f87e614303612c4
Mati <matibat@gmail.com>
2022-04-29 18:57:55 -0300

    chore(common): renames cached value PE-1340

--- src/common.ts
+++ src/common.ts
@@ -2,14 +2,14 @@ export function gqlResultName(minBlock: number, maxBlock: number): string {
 	return `gql_result_${minBlock}-${maxBlock}.json`;
 }
 
-let _blockHeight: number | undefined;
+let _cachedBlockHeight: number | undefined;
 export async function getBlockHeight(): Promise<number> {
-	if (_blockHeight) {
-		return _blockHeight;
+	if (_cachedBlockHeight) {
+		return _cachedBlockHeight;
 	}
 	const blockHeightRequest = await fetch('https://arweave.net/height');
 	const blockHeightBlob = await blockHeightRequest.blob();
 	const blockHeightText = await blockHeightBlob.text();
 	const blockHeight = +blockHeightText;
-	return (_blockHeight = blockHeight);
+	return (_cachedBlockHeight = blockHeight);
 }
