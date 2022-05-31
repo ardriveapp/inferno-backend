f96f851da7a54e1aef48a2a1d5aadda96161059b
Mati <matibat@gmail.com>
2022-05-31 18:56:34 -0300

    feat(tags): minor fix in the tag parsing and filter PE-1645

--- src/utils/layer_one_helpers.ts
+++ src/utils/layer_one_helpers.ts
@@ -3,7 +3,7 @@ import { APP_NAME_TAG, VALID_APP_NAMES } from '../constants';
 import { GQLEdgeInterface, GQLTagInterface } from '../gql_types';
 
 export function ardriveTxFilter(edge: GQLEdgeInterface) {
-	const appName = edge.node.tags.find((tag) => tag.name === APP_NAME_TAG)?.name;
+	const appName = edge.node.tags.find((tag) => tag.name === APP_NAME_TAG)?.value;
 	return !!appName && VALID_APP_NAMES.includes(appName);
 }
 
@@ -11,8 +11,8 @@ export function decodeTags(encodedTags: GQLTagInterface[]): GQLTagInterface[] {
 	const tags = encodedTags.map((tag) => {
 		try {
 			return {
-				name: fromB64Url(tag.name).toString().toLowerCase(),
-				value: fromB64Url(tag.name).toString()
+				name: fromB64Url(tag.name).toString(),
+				value: fromB64Url(tag.value).toString()
 			};
 		} catch (error) {
 			return { name: 'undefined', value: 'undefined' };
