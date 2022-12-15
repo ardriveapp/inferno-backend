40c9cfbcd1e56e7c5048dcc07d69da3a6faddae4
Mati <matibat@gmail.com>
2022-12-15 20:06:48 -0300

    chore(types): be explicit with method's return type PE-2771

--- src/utils/layer_1_helpers.ts
+++ src/utils/layer_1_helpers.ts
@@ -2,7 +2,7 @@ import { createHash } from 'crypto';
 import { APP_NAME_TAG, VALID_APP_NAMES } from '../constants';
 import { GQLEdgeInterface, GQLTagInterface } from '../gql_types';
 
-export function ardriveTxFilter(edge: GQLEdgeInterface) {
+export function ardriveTxFilter(edge: GQLEdgeInterface): boolean {
 	const appNameTags = edge.node.tags.filter((tag) => tag.name === APP_NAME_TAG);
 	return VALID_APP_NAMES.some((validAppName) => appNameTags.find((tag) => tag.value === validAppName));
 }
@@ -21,11 +21,11 @@ export function decodeTags(encodedTags: GQLTagInterface[]): GQLTagInterface[] {
 	return tags;
 }
 
-export function sha256B64Url(input: Buffer) {
+export function sha256B64Url(input: Buffer): string {
 	return toB64url(createHash('sha256').update(input).digest());
 }
 
-export function toB64url(buffer: Buffer) {
+export function toB64url(buffer: Buffer): string {
 	return buffer.toString('base64').replace(/\+/g, '-').replace(/\//g, '_').replace(/=/g, '');
 }
 
